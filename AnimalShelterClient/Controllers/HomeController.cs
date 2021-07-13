e5f1f912b7fed5ad46f56b6b25010380a159b6bf
Chris Ramer <chrisramer.97@gmail.com>
2021-07-13 10:55:33 -0700

    Use versioning

--- AnimalShelterClient/Controllers/HomeController.cs
+++ AnimalShelterClient/Controllers/HomeController.cs
@@ -7,10 +7,30 @@ namespace AnimalShelterClient.Controllers
 {
 	public class HomeController : Controller
 	{
+		static List<string> apiVersions = new List<string>()
+		{
+			"1.0",
+			"2.0"
+		};
+
 		[HttpGet("/")]
 		public ActionResult Index()
 		{
+			List<SelectListItem> versions = apiVersions.ConvertAll(v => new SelectListItem
+			{
+				Text = v,
+				Value = v,
+			});
+			ViewBag.VersionList = versions;
 			return View();
+		}
+
+		[HttpPost("/")]
+		public ActionResult Index(SelectListItem test)
+		{
+			double version = double.Parse(Request.Form["VersionList"]);
+			ApiHelper.ApiVersion = version;
+			return RedirectToAction("Index", "Animals", new { version = version });
 		}
 	}
 }
