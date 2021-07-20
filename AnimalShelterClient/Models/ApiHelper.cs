9602fb0a526dbf3a378fb3723b7d954cd3354641
Chris Ramer <chrisramer.97@gmail.com>
2021-07-20 11:42:32 -0700

    Replace link logic properties with functions

--- AnimalShelterClient/Models/ApiHelper.cs
+++ AnimalShelterClient/Models/ApiHelper.cs
@@ -5,22 +5,31 @@ namespace AnimalShelterClient.Models
 {
 	public class ApiHelper
 	{
-		static double apiVersion = 1;
-		public static double ApiVersion
+		public static double ApiVersion { get; set; }
+
+		public static bool HasReadPermission()
 		{
-			get
-			{
-				return apiVersion;
-			}
-			set
-			{
-				apiVersion = value;
-			}
+			return ApiVersion > 1;
+		}
+
+		public static bool HasCreatePermission()
+		{
+			return ApiVersion > 2;
+		}
+
+		public static bool HasUpdatePermission()
+		{
+			return ApiVersion > 3;
+		}
+
+		public static bool HasDeletePermission()
+		{
+			return ApiVersion > 4;
 		}
 
 		public static async Task<string> GetAll()
 		{
-			RestClient client = new RestClient($"http://localhost:5004/v{apiVersion}");
+			RestClient client = new RestClient($"http://localhost:5004/v{ApiVersion}");
 			RestRequest request = new RestRequest("/animals", Method.GET);
 			IRestResponse response = await client.ExecuteTaskAsync(request);
 			return response.Content;
@@ -28,7 +37,7 @@ namespace AnimalShelterClient.Models
 
 		public static async Task<string> Get(int id)
 		{
-			RestClient client = new RestClient($"http://localhost:5004/v{apiVersion}");
+			RestClient client = new RestClient($"http://localhost:5004/v{ApiVersion}");
 			RestRequest request = new RestRequest($"/animals/{id}", Method.GET);
 			IRestResponse response = await client.ExecuteTaskAsync(request);
 			return response.Content;
@@ -36,7 +45,7 @@ namespace AnimalShelterClient.Models
 
 		public static async Task Post(string animal)
 		{
-			RestClient client = new RestClient($"http://localhost:5004/v{apiVersion}");
+			RestClient client = new RestClient($"http://localhost:5004/v{ApiVersion}");
 			RestRequest request = new RestRequest("/animals", Method.POST);
 			request.AddHeader("Content-Type", "application/json");
 			request.AddJsonBody(animal);
@@ -45,7 +54,7 @@ namespace AnimalShelterClient.Models
 
 		public static async Task Put(int id, string animal)
 		{
-			RestClient client = new RestClient($"http://localhost:5004/v{apiVersion}");
+			RestClient client = new RestClient($"http://localhost:5004/v{ApiVersion}");
 			RestRequest request = new RestRequest($"/animals/{id}", Method.PUT);
 			request.AddHeader("Content-Type", "application/json");
 			request.AddJsonBody(animal);
@@ -54,7 +63,7 @@ namespace AnimalShelterClient.Models
 
 		public static async Task Delete(int id)
 		{
-			RestClient client = new RestClient($"http://localhost:5004/v{apiVersion}");
+			RestClient client = new RestClient($"http://localhost:5004/v{ApiVersion}");
 			RestRequest request = new RestRequest($"/animals/{id}", Method.DELETE);
 			request.AddHeader("Content-Type", "application/json");
 			IRestResponse response = await client.ExecuteTaskAsync(request);
